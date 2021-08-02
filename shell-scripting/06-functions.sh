66c8e503e0148f1362843b10b27f7e6952f35454
MYPC <yashwanthkotha147@gmail.com>
2021-08-02 16:50:25 +0530

    commands are updated...

--- shell-scripting/06-functions.sh
+++ shell-scripting/06-functions.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/bash
-sample() {
-  echo Hello, I am a sample function
+#sample() {
+#  echo Hello, I am a sample function
+#}
+#sample
+
+sample(){
+  echo value of a=$a
 }
+a=100
 sample
+
+sample1(){
+  b=566876
+}
+sample1
+echo value of b=$b
