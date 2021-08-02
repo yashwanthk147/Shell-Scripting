21809577b5088e90d78431a9033f6a3573113849
MYPC <yashwanthkotha147@gmail.com>
2021-08-02 15:58:31 +0530

    commands are updated.

--- shell-scripting/02-print.sh
+++ shell-scripting/02-print.sh
@@ -2,3 +2,4 @@
 #display message to users
 #echo Hello world my name is Yashwanth Reddy
 echo -e "Hello world,\n\tMy name is Yashwanth Reddy"
+echo -e "\e[36mHello world,\n\tMy name is Yashwanth Reddy IN CYAN"
