c2485936d307ee18590348169b7355831411cd75
MYPC <yashwanthkotha147@gmail.com>
2021-08-03 17:12:12 +0530

    files are added...

--- shell-scripting/roboshop/frontend.sh
+++ shell-scripting/roboshop/frontend.sh
@@ -6,10 +6,12 @@ rm -f $LOG
 echo -e "Installing nginx \e[32mdone\e[0m"
 yum install nginx -y   &>>$LOG
 # if you given &> then output and error will be going in to the same files.
+echo $?
 
 echo "Enabling nginx"
 systemctl enable nginx &>>$LOG
+echo $?
 
 echo "Starting nginx"
 systemctl start nginx &>>$LOG
-
+echo $?
