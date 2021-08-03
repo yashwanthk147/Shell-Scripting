dfd46ed0b63625fe838f26aa879790a455ed5df9
MYPC <yashwanthkotha147@gmail.com>
2021-08-03 17:25:57 +0530

    files are added...

--- shell-scripting/roboshop/frontend.sh
+++ shell-scripting/roboshop/frontend.sh
@@ -6,7 +6,7 @@ rm -f $LOG
 echo -e "Installing nginx\t\t\t..."
 yum install nginx -y   &>>$LOG
 # if you given &> then output and error will be going in to the same files.
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo done
 else
   echo fail
