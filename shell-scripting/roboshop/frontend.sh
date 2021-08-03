e081b13ef037c8a3ea55d7412e42596e5bf5fd33
MYPC <yashwanthkotha147@gmail.com>
2021-08-03 17:42:02 +0530

    files are added...

--- shell-scripting/roboshop/frontend.sh
+++ shell-scripting/roboshop/frontend.sh
@@ -3,30 +3,24 @@
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
+STAT_CHECK{
+  if [ $1 -eq 0 ]; then
+  echo done
+else
+  echo fail
+  exit 1
+fi
+}
+
 echo -n -e "Installing nginx\t\t\t..."
 yum install nginx -y   &>>$LOG
 # if you given &> then output and error will be going in to the same files.
-if [ $? -eq 0 ]; then
-  echo done
-else
-  echo fail
-fi
+STAT_CHECK $?
 
 echo -n -e "Enabling nginx\t\t\t..."
-echo "Enabling nginx"
 systemctl enable nginx &>>$LOG
-if [ $? -eq 0 ]; then
-  echo done
-else
-  echo fail
-fi
+STAT_CHECK $?
 
 echo -n -e "Starting nginx\t\t\t..."
-echo "Starting nginx"
 systemctl start nginx &>>$LOG
-if [ $? -eq 0 ]; then
-  echo done
-else
-  echo fail
-fi
-
+STAT_CHECK $?
