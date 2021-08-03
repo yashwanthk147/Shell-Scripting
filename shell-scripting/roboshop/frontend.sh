963281625f263c7d5bac52bb3d05f164a466dc05
MYPC <yashwanthkotha147@gmail.com>
2021-08-03 17:50:47 +0530

    files are added...

--- shell-scripting/roboshop/frontend.sh
+++ shell-scripting/roboshop/frontend.sh
@@ -20,10 +20,10 @@ yum install nginx -y   &>>$LOG
 # if you given &> then output and error will be going in to the same files.
 STAT_CHECK $?
 
-PRINT -n -e "Enabling nginx"
+PRINT -n -e "Enabling nginx\t"
 systemctl enable nginx &>>$LOG
 STAT_CHECK $?
 
-PRINT -n -e "Starting nginx"
+PRINT -n -e "Starting nginx\t"
 systemctl start nginx &>>$LOG
 STAT_CHECK $?
