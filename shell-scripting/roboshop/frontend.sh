cb795774fb0b9c9bc54f1b42b00601db1463ff70
MYPC <yashwanthkotha147@gmail.com>
2021-08-03 18:27:30 +0530

    files are added...

--- shell-scripting/roboshop/frontend.sh
+++ shell-scripting/roboshop/frontend.sh
@@ -7,6 +7,22 @@ yum install nginx -y   &>>$LOG
 # if you given &> then output and error will be going in to the same files.
 STAT_CHECK $?
 
+PRINT "Downloading Frontend"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+PRINT "Remove old Htdocs"
+cd /usr/share/nginx/html && rm -rf * &>>$LOG
+STAT_CHECK $?
+
+PRINT "Extract frontend archive"
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* &>>$LOG && mv static/* &>>$LOG &&rm -rf frontend-master static &>>$LOG
+STAT_CHECK $?
+
+PRINT "Update roboshop config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+STAT_CHECK $?
+
 PRINT "Enabling nginx\t"
 systemctl enable nginx &>>$LOG
 STAT_CHECK $?
