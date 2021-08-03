a87ea4bd609a4538c0b7d36dcb6017491ef82f9e
MYPC <yashwanthkotha147@gmail.com>
2021-08-03 15:46:43 +0530

    files are added...

--- shell-scripting/roboshop/frontend.sh
+++ shell-scripting/roboshop/frontend.sh
@@ -4,11 +4,12 @@ LOG=/tmp/roboshop.log
 rm -f $LOG
 
 echo -e "Installing nginx \e[32mdone\e[0m"
-yum install nginx -y   >>$LOG
+yum install nginx -y   &>>$LOG
+# if you given &> then output and error will be going in to the same files.
 
 echo "Enabling nginx"
-systemctl enable nginx >>$LOG
+systemctl enable nginx &>>$LOG
 
 echo "Starting nginx"
-systemctl start nginx >>$LOG
+systemctl start nginx &>>$LOG
 
