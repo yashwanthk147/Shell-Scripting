da322f9ecfaaf000ee37a326c28af979d8ae0a4a
MYPC <yashwanthkotha147@gmail.com>
2021-08-04 17:32:14 +0530

    files are added...

---
+++ shell-scripting/roboshop/catalogue.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+common source.sh
+
+PRINT "Install NodeJS"
+yum install nodejs make gcc-c++ -y &>>$LOG
+STAT_CHECK $?
+
+PRINT "Add Roboshop Application User"
+useradd roboshop &>>$LOG
+STAT_CHECK $?
+
+
+
+#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+#$ cd /home/roboshop
+#$ unzip /tmp/catalogue.zip
+#$ mv catalogue-main catalogue
+#$ cd /home/roboshop/catalogue
+#$ npm install
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
