a9e18156f564dee915f9a04e55b329f7b05e1490
MYPC <yashwanthkotha147@gmail.com>
2021-08-04 19:38:20 +0530

    files are added...

--- shell-scripting/roboshop/catalogue.sh
+++ shell-scripting/roboshop/catalogue.sh
@@ -31,8 +31,12 @@ PRINT "Fix Application Permissions"
 chown roboshop:roboshop /home/roboshop -R  &>>$LOG
 STAT_CHECK $?
 
-PRINT "Update SystemD file\t"
+PRINT "copy SystemD file\t"
 sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+STAT_CHECK $?
+
+PRINT "Update DNS record"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' /etc/systemd/system/catalogue.service
 STAT_CHECK $?
 
 PRINT "Start catalogue service\t\t"
