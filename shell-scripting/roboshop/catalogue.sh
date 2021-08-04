6805a9bd12fbc6ab01049dba4acd6f5aa5e7cada
MYPC <yashwanthkotha147@gmail.com>
2021-08-04 18:18:41 +0530

    files are added...

--- shell-scripting/roboshop/catalogue.sh
+++ shell-scripting/roboshop/catalogue.sh
@@ -18,7 +18,7 @@ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/cat
 STAT_CHECK $?
 
 PRINT "Extract Downloaded content"
-cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install
+cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install --unsafe-perm
 STAT_CHECK $?
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
