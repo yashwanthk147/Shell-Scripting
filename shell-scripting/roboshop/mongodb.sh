f16668bd2b8dc3b214617db2809e7834f4499016
MYPC <yashwanthkotha147@gmail.com>
2021-08-04 11:20:49 +0530

    files are added...

---
+++ shell-scripting/roboshop/mongodb.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+source commmon.sh
+
+PRINT "Setup mongodb repository\t\t..."
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+STAT_CHECK $?
+
+
+# yum install -y mongodb-org
+# systemctl enable mongod
+# systemctl start mongod
+#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
+#Config file: /etc/mongod.conf
+#
+#then restart the service
+
+# systemctl restart mongod
+
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js
