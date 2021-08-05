9a197805017db1b9fa04eced823375813c12cd3a
MYPC <yashwanthkotha147@gmail.com>
2021-08-05 11:18:30 +0530

    files are added...

--- shell-scripting/roboshop/mongodb.sh
+++ shell-scripting/roboshop/mongodb.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 
 source common.sh
-#
-#PRINT "Setting Up repository\t"
-#echo '[mongodb-org-4.2]
-#name=MongoDB Repository
-#baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
-#gpgcheck=1
-#enabled=1
-#gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-#STAT_CHECK $?
-#
+
+PRINT "Setting Up repository\t"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+STAT_CHECK $?
+
 #PRINT "Install MongoDB\t\t"
 #yum install -y mongodb-org &>>$LOG
 #STAT_CHECK $?
