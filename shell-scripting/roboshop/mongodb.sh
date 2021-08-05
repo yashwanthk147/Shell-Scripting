d6cb16cd5132dafdb1851fab44ba85d07c68639b
MYPC <yashwanthkotha147@gmail.com>
2021-08-05 11:40:00 +0530

    files are added...

--- shell-scripting/roboshop/mongodb.sh
+++ shell-scripting/roboshop/mongodb.sh
@@ -15,9 +15,9 @@ PRINT "Install MongoDB\t\t"
 yum install -y mongodb-org &>>$LOG
 STAT_CHECK $?
 #
-#PRINT "Update MongoDB Listen Address"
-#sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG
-#STAT_CHECK $?
+PRINT "Update MongoDB Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG
+STAT_CHECK $?
 #
 #PRINT "START MongoDB Service\t"
 #systemctl enable mongod &>>$LOG && systemctl start mongod &>>$LOG
