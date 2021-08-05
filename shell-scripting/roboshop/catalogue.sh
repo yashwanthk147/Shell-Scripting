58e86f369ee3f75a8d3e6e5f0e898179f91eacea
MYPC <yashwanthkotha147@gmail.com>
2021-08-05 12:14:04 +0530

    files are added...

--- shell-scripting/roboshop/catalogue.sh
+++ shell-scripting/roboshop/catalogue.sh
@@ -7,24 +7,24 @@ yum install nodejs make gcc-c++ -y &>>$LOG
 STAT_CHECK $?
 
 PRINT "Add Roboshop Application User"
-#id roboshop &>>$LOG
-#if [ $? -ne 0 ]; then                    #usernot exist then adding the user
-#  useradd roboshop &>>$LOG
-#fi
-#STAT_CHECK $?
-#
-#PRINT "Download Application Code"
-#curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-#STAT_CHECK $?
-#
-#PRINT "Extract Downloaded content"                      #remove the old content and new content to the old directory, because it shows catalogue aleready exist
-#cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>$LOG && rm -rf catalogue && mv catalogue-main catalogue
+id roboshop &>>$LOG
+if [ $? -ne 0 ]; then                    #usernot exist then adding the user
+  useradd roboshop &>>$LOG
+fi
+STAT_CHECK $?
+
+PRINT "Download Application Code"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+STAT_CHECK $?
+
+PRINT "Extract Downloaded content"                      #remove the old content and new content to the old directory, because it shows catalogue aleready exist
+cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>$LOG && rm -rf catalogue && mv catalogue-main catalogue
 ##I'm running the script with root user. so, i'm getting acess denied. To overcome these i'm giving unsafe-perm
-#STAT_CHECK $?
-#
-#PRINT "Install NodeJS dependencies"
-#cd /home/roboshop/catalogue && npm install --unsafe-perm &>>$LOG
-#STAT_CHECK $?
+STAT_CHECK $?
+
+PRINT "Install NodeJS dependencies"
+cd /home/roboshop/catalogue && npm install --unsafe-perm &>>$LOG
+STAT_CHECK $?
 #
 #PRINT "Fix Application Permissions"
 ##changing the root user as roboshop user
