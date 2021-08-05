988e85e24ec8757e021556cacf932d084b3d31eb
MYPC <yashwanthkotha147@gmail.com>
2021-08-05 14:36:30 +0530

    files are added...

--- shell-scripting/roboshop/common.sh
+++ shell-scripting/roboshop/common.sh
@@ -2,7 +2,7 @@
 
 USER_ID=$(id -u)
 if [ $USER-ID -ne 0 ]; then
-  echo -e " You should be the root user / sudo user too run this script"
+  echo -e "You should be the root user / sudo user too run this script"
   exit 2
 fi
 
@@ -14,7 +14,7 @@ STAT_CHECK() {
   echo done
 else
   echo fail
-  echo -e " Check the log file more details, Log File - $LOG"
+  echo -e "Check the log file more details, Log File - $LOG"
   exit 1
 fi
 }
