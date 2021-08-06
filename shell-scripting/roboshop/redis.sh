3b29b054a8d1a4fbcaadb9eb5bcd1bf4a83cb86b
MYPC <yashwanthkotha147@gmail.com>
2021-08-06 18:35:54 +0530

    files are added...

--- shell-scripting/roboshop/redis.sh
+++ shell-scripting/roboshop/redis.sh
@@ -15,6 +15,6 @@ STAT_CHECK $?
 
 #Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
 
-PRINT "Restart Redis Database"
-systemctl enable redis &>>$LOG && systemctl restart redis &>>$LOG
+PRINT "start Redis Database"
+systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
 STAT_CHECK $?
