cb683d0d85fd53794c9606f0f7e08131da7d13a7
MYPC <yashwanthkotha147@gmail.com>
2021-08-06 18:26:13 +0530

    files are added...

---
+++ shell-scripting/roboshop/redis.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+source common.sh
+
+PRINT "Install Redis Repos\t"
+yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
+STAT_CHECK $?
+
+PRINT "Install Redis\t"
+yum install redis -y --enablerepo=remi &>>$LOG
+
+PRINT "Update Redis Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf
+STAT_CHECK $?
+
+#Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
+
+PRINT "Restart Redis Database"
+systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
+STAT_CHECK $?
