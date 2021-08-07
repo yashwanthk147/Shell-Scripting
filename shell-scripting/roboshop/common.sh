ad2e5647f98a59e25a3bea9b186011522a65ca41
MYPC <yashwanthkotha147@gmail.com>
2021-08-07 12:38:46 +0530

    files are added...

--- shell-scripting/roboshop/common.sh
+++ shell-scripting/roboshop/common.sh
@@ -18,18 +18,16 @@ PRINT() {
   echo -n -e "$1\t\t..."
 }
 
-NODEJS() {
-  PRINT "Install NodeJS"
-  yum install nodejs make gcc-c++ -y &>>$LOG
-  STAT_CHECK $?
-
+ADD_APPLICCATION_USER() {
   PRINT "Add Roboshop Application User"
   id roboshop &>>$LOG
   if [ $? -ne 0 ]; then                    #usernot exist then adding the user
     useradd roboshop &>>$LOG
   fi
   STAT_CHECK $?
+}
 
+DOWNLOAD_APP_CODE(){
   PRINT "Download Application Code"
   curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip"
   STAT_CHECK $?
@@ -38,21 +36,65 @@ NODEJS() {
   cd /home/roboshop && unzip -o /tmp/${COMPONENT}.zip &>>$LOG && rm -rf ${COMPONENT} && mv ${COMPONENT}-main ${COMPONENT}
   ##I'm running the script with root user. so, i'm getting acess denied. To overcome these i'm giving unsafe-perm
   STAT_CHECK $?
+}
 
-  PRINT "Install NodeJS dependencies"
-  cd /home/roboshop/${COMPONENT} && npm install --unsafe-perm &>>$LOG
-  STAT_CHECK $?
-
+FIX_PERMI(){
   PRINT "Fix Application Permissions"
   ##changing the root user as roboshop user
   chown roboshop:roboshop /home/roboshop -R  &>>$LOG
   STAT_CHECK $?
+}
 
+SETUP_SYSTEMD(){
   PRINT "setup SystemD file\t"
-  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e
+  "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" -e
+  "s/CART_ENDPOINT/cart.roboshop.internal/" -e "s/DBHOST/mysql.roboshop.internal/" /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
   STAT_CHECK $?
 
   PRINT "Start ${COMPONENT} service\t\t"
   systemctl daemon-reload &>>$LOG && systemctl start ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
   STAT_CHECK $?
+}
+
+NODEJS() {
+  PRINT "Install NodeJS"
+  yum install nodejs make gcc-c++ -y &>>$LOG
+  STAT_CHECK $?
+
+  ADD_APPLICCATION_USER
+  DOWNLOAD_APP_CODE
+
+  PRINT "Install NodeJS dependencies"
+  cd /home/roboshop/${COMPONENT} && npm install --unsafe-perm &>>$LOG
+  STAT_CHECK $?
+
+  FIX_PERMI
+  SETUP_SYSTEMD
+
+
+}
+
+JAVA() {
+  PRINT "Install Maven"
+  yum install maven -y
+  STAT_CHECK $?
+
+  ADD_APPLICCATION_USER
+  DOWNLOAD_APP_CODE
+
+  PRINT "compile code"
+  cd /home/roboshop/${COMPOMNENT} && mv clean package &>>$LOG && mv target/shipping-1.0.jar shipping.jar
+  STAT_CHECK $?
+
+  FIX_PERMI
+  SETUP_SYSTEMD
+
+
+
+#
+## mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
+## systemctl daemon-reload
+## systemctl start shipping
+## systemctl enable shipping
 }
