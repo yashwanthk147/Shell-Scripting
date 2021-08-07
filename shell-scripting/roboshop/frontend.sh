1ece41d619df5fb0f505ea5720b5b6d64770c88a
MYPC <yashwanthkotha147@gmail.com>
2021-08-07 13:10:36 +0530

    files are added...

--- shell-scripting/roboshop/frontend.sh
+++ shell-scripting/roboshop/frontend.sh
@@ -2,29 +2,30 @@
 
 source common.sh
 
-PRINT "Installing nginx"
-yum install nginx -y   &>>$LOG
-# if you given &> then output and error will be going in to the same files.
+
+PRINT "Installing Nginx\t"
+yum install nginx -y &>>$LOG
 STAT_CHECK $?
 
-PRINT "Downloading Frontend"
+PRINT "Download Frontend\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 STAT_CHECK $?
 
-PRINT "Remove old Htdocs"
-cd /usr/share/nginx/html && rm -rf * &>>$LOG
+
+PRINT "Remove Old HtDocs\t"
+cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
 STAT_CHECK $?
 
 PRINT "Extract Frontend Archive"
-unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG &&rm -rf frontend-master static &>>$LOG
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG  && rm -rf frontend-master static &>>$LOG
 STAT_CHECK $?
 
-PRINT "Copy RoboShop config\t"
+PRINT "Copy RoboShop Config\t"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 STAT_CHECK $?
 
 PRINT "Update RoboShop Config\t"
-sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/'  -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/'  -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 STAT_CHECK $?
 
 PRINT "Enabling Nginx\t\t"
